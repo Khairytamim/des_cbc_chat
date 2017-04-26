28065e3db9364a0df78fefdc013d4463d78d0ebc
fourirakbar <fourir.akbar@gmail.com>
2017-04-26 19:46:28 +0700

    fix client

--- client.py
+++ client.py
@@ -1,22 +1,57 @@
 import socket
 from DES import *
 
+q = 353
+a = 3
+
+xb = input('masukkan random key untuk bob (xb) : ')
+int_xb = int(xb)
+yb = (a**int_xb)%q
+jebret = str(yb)
+print('ini yb-> ', yb)
+
+xxx = 0
+
 s = socket.socket()
 host = socket.gethostname()
-port = 12228
+port = 12222
 
 s.connect((host, port))
 print 'Terhubung dengan server dengan IP : ', host
 
 while True:
-    z = raw_input("Tulis chat ke server: ")
-    z_encrypt = encryptdua(z)
-    s.send(z_encrypt)
-    print 'Menunggu response'
+	xxx = xxx + 1
 
-    x = s.recv(1024)
-    print "yg blm di dekrip = ", x
+	print ("jembut-> ", xxx)
+	if xxx == 1:
+		s.sendall(jebret)
+		ya = s.recv(10)
+		print ('ini ya->', ya)
+		kab = (int(ya)**int_xb)%q
+		print ('ini kab->', kab)
+		jebrets = str(kab)
 
-    pesan_server = decrypt(x)
-    print "Pesan dari server -> ", pesan_server
+		tambah_doang = 0
+
+		if len(jebrets) < 8:
+			temp_jebrets = len(jebrets)
+			while temp_jebrets < 8:
+				temp_jebrets = temp_jebrets + 1
+				jebrets = jebrets + str(tambah_doang)
+		print jebrets
+		with open('key_dh.txt','wb') as f:
+			data = f.write(jebrets)
+		print ("jebrets, sudah ditulis ke key_dh-> ", jebrets)
+
+
+	z = raw_input("Tulis chat ke server: ")
+	z_encrypt = encryptdua(z)
+	s.send(z_encrypt)
+	print 'Menunggu response'
+
+	x = s.recv(1024)
+	print "yg blm di dekrip = ", x
+
+	pesan_server = decrypt(x)
+	print "Pesan dari server -> ", pesan_server
 
